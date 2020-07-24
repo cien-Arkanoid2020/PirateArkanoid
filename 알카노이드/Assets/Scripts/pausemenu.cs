a31ab26024aa188a760ba8f41522257ae82c94ee
ArcRoot <phg5253@naver.com>
2020-07-24 21:37:01 +0900

    레벨 파일 수정

---
+++ 알카노이드/Assets/Scripts/pausemenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+    private bool paused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+        if(paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if(!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    public void Resume()
+    {
+        paused = !paused;
+    }
+}
